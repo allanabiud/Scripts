68599985017dcf1898b166566525f5b857c15154
allanabiud <nyagaka254@gmail.com>
2025-10-27 17:58:12 +0300

    reorganize repo

--- godot-scripts/godot-nvim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PIPE="$HOME/.cache/nvim/godot.pipe"
-
-if [ -n "$1" ]; then
-  file="$1"
-  nvim --server "$PIPE" --remote-send ":e ${file}<CR>"
-fi
--- godot-scripts/gvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PIPE="$HOME/.cache/nvim/godot.pipe"
-
-# Ensure the directory exists
-mkdir -p "$(dirname "$PIPE")"
-
-# Start nvim in project root, listening on the pipe
-nvim --listen "$PIPE" .
